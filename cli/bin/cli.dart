6877a6b7511cb30cb96d1c71790ddeda4e253a14
littlehorseboy <littlehorseboy@gmail.com>
2026-04-13 22:28:52 +0800

    feat: Remove unused import and update greeting message in cli.dart

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -1,5 +1,5 @@
-import 'package:cli/cli.dart' as cli;
+import 'package:cli/cli.dart' as cli; // Delete this entire line
 
 void main(List<String> arguments) {
-  print('Hello world: ${cli.calculate()}!');
+  print('Hello, Dart!'); // Change this line
 }
