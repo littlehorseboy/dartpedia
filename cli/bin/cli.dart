3eb88980b96f0c19b3ede22ae51f229f8394eff7
littlehorseboy <littlehorseboy@gmail.com>
2026-04-13 22:31:53 +0800

    feat: Update CLI to include versioning and usage instructions

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -1,5 +1,20 @@
 import 'package:cli/cli.dart' as cli; // Delete this entire line
 
+const version = '0.0.1'; // Add this line
+
 void main(List<String> arguments) {
-  print('Hello, Dart!'); // Change this line
+  if (arguments.isEmpty || arguments.first == 'help') {
+    printUsage(); // Change this from 'Hello, Dart!'
+  } else if (arguments.first == 'version') {
+    print('Dartpedia CLI version $version');
+  } else {
+    printUsage(); // Catch-all for any unrecognized command.
+  }
+}
+
+void printUsage() {
+  // Add this new function
+  print(
+    "The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'",
+  );
 }
