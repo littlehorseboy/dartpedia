83bca064f669b4d2df1607c485042a38ac673330
littlehorseboy <littlehorseboy@gmail.com>
2026-04-13 22:35:30 +0800

    feat: Add search functionality and usage instructions to CLI

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -4,12 +4,22 @@ const version = '0.0.1'; // Add this line
 
 void main(List<String> arguments) {
   if (arguments.isEmpty || arguments.first == 'help') {
-    printUsage(); // Change this from 'Hello, Dart!'
+    printUsage();
   } else if (arguments.first == 'version') {
     print('Dartpedia CLI version $version');
+  } else if (arguments.first == 'search') {
+    // Add this new block:
+    final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
+    searchWikipedia(inputArgs);
   } else {
-    printUsage(); // Catch-all for any unrecognized command.
+    printUsage();
   }
+}
+
+
+void searchWikipedia(List<String>? arguments) {
+  // Add this new function and add ? to arguments type
+  print('searchWikipedia received arguments: $arguments');
 }
 
 void printUsage() {
